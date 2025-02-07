80fc774202937248415d8f6978c61616ff21993b
AI0595-Dhanasekar <dhanasekar@aggregateintelligence.in>
2025-02-07 17:22:21 +0530

    Update tips_sample.py

--- tips_sample.py
+++ tips_sample.py
@@ -6,8 +6,8 @@ http = urllib3.PoolManager()
 
 response = http.request("GET", "https://www.expedia.com/", headers={"User-Agent": "Mozilla/5.0"})
 
-prin = response.headers.get("Set-Cookie")
-print(prin)
+printes = response.headers.get("Set-Cookie")
+print(printes)
 
 
 
